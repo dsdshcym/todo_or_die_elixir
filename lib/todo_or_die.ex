21105e3150f3008c22e66495a09955f0bdce9dfa
Yiming Chen <dsdshcym@gmail.com>
2019-01-04 12:03:18 +0800

    chore: remove TodoOrDie.hello/1 generated by `mix new`

--- lib/todo_or_die.ex
+++ lib/todo_or_die.ex
@@ -1,18 +1,2 @@
 defmodule TodoOrDie do
-  @moduledoc """
-  Documentation for TodoOrDie.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TodoOrDie.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/todo_or_die_test.exs
+++ test/todo_or_die_test.exs
@@ -1,8 +1,4 @@
 defmodule TodoOrDieTest do
   use ExUnit.Case
   doctest TodoOrDie
-
-  test "greets the world" do
-    assert TodoOrDie.hello() == :world
-  end
 end
