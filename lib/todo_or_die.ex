525894d6d39864baf5588d8e3282761a81b99ef7
Yiming Chen <dsdshcym@gmail.com>
2019-01-05 15:13:06 +0800

    refactor: use TodoOrDie.OverdueError to replace RuntimeError in due/{2,3}

--- lib/todo_or_die.ex
+++ lib/todo_or_die.ex
@@ -3,11 +3,11 @@ defmodule TodoOrDie do
   Raises an exception when the task is overdue.
 
   iex> TodoOrDie.due("Remove this", by: ~D[2019-01-01])
-  ** (RuntimeError) TODO: "Remove this" came due on 2019-01-01. Do it!
+  ** (TodoOrDie.OverdueError) TODO: "Remove this" came due on 2019-01-01. Do it!
   """
   def due(description, today \\ Date.utc_today(), by: due_at) do
     if Date.compare(today, due_at) != :lt do
-      raise "TODO: \"#{description}\" came due on #{due_at}. Do it!"
+      raise TodoOrDie.OverdueError, todo: description, due_at: due_at
     else
       :ok
     end
--- test/todo_or_die_test.exs
+++ test/todo_or_die_test.exs
@@ -2,25 +2,27 @@ defmodule TodoOrDieTest do
   use ExUnit.Case
   doctest TodoOrDie
 
+  alias TodoOrDie.OverdueError
+
   describe "due/3" do
     test "doesn't raise when today is before due date" do
       assert TodoOrDie.due("Fix Me", ~D[2018-12-31], by: ~D[2019-12-31])
     end
 
     test "raises when today is due date" do
-      assert_raise RuntimeError, fn ->
+      assert_raise OverdueError, fn ->
         TodoOrDie.due("Fix Me", ~D[2018-12-31], by: ~D[2018-12-31])
       end
     end
 
     test "raises when today is beyond due date" do
-      assert_raise RuntimeError, fn ->
+      assert_raise OverdueError, fn ->
         TodoOrDie.due("Fix Me", ~D[2019-01-01], by: ~D[2018-12-31])
       end
     end
 
     test "raises customized message based on input" do
-      assert_raise RuntimeError, "TODO: \"Fix Me\" came due on 2019-06-23. Do it!", fn ->
+      assert_raise OverdueError, "TODO: \"Fix Me\" came due on 2019-06-23. Do it!", fn ->
         TodoOrDie.due("Fix Me", ~D[2020-01-01], by: ~D[2019-06-23])
       end
     end
