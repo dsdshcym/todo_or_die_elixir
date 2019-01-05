2d6f3c3f8630a3d4502d6613bf01a821d67eb589
Yiming Chen <dsdshcym@gmail.com>
2019-01-05 14:53:09 +0800

    feat: add TodoOrDie.due/{2,3}

--- lib/todo_or_die.ex
+++ lib/todo_or_die.ex
@@ -1,2 +1,15 @@
 defmodule TodoOrDie do
+  @doc """
+  Raises an exception when the task is overdue.
+
+  iex> TodoOrDie.due("Remove this", by: ~D[2019-01-01])
+  ** (RuntimeError) TODO: "Remove this" came due on 2019-01-01. Do it!
+  """
+  def due(description, today \\ Date.utc_today(), by: due_at) do
+    if Date.compare(today, due_at) != :lt do
+      raise "TODO: \"#{description}\" came due on #{due_at}. Do it!"
+    else
+      :ok
+    end
+  end
 end
