9ef32786f928ed1209b194637348e28d17c03c8d
Yiming Chen <dsdshcym@gmail.com>
2019-02-05 16:42:52 +0800

    docs: indent example for TodoOrDie.due
    - so that it can be exported as a code block

--- lib/todo_or_die.ex
+++ lib/todo_or_die.ex
@@ -2,8 +2,10 @@ defmodule TodoOrDie do
   @doc """
   Raises an exception when the task is overdue.
 
-  iex> TodoOrDie.due("Remove this", by: ~D[2019-01-01])
-  ** (TodoOrDie.OverdueError) TODO: "Remove this" came due on 2019-01-01. Do it!
+  ## Examples
+
+       iex> TodoOrDie.due("Remove this", by: ~D[2019-01-01])
+       ** (TodoOrDie.OverdueError) TODO: "Remove this" came due on 2019-01-01. Do it!
   """
   def due(description, today \\ Date.utc_today(), by: due_at) do
     if Date.compare(today, due_at) != :lt do
